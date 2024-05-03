a97084fc5d5870cb5c7c7d89624bd40df5ec78fd
Michael Daley <daleyhockey9@gmail.com>
2024-05-02 23:39:21 -0400

    Some more style

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,26 +1,18 @@
-import Link from 'next/link';
-
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-md bg-blue-200 p-2 md:h-10 border-md border-blue-500">
-        <text className="font-semibold text-xl text-slate-700">WPGR</text>
+    <main className="flex min-h-screen flex-col p-6 from-green-800 to-yellow-700 bg-gradient-to-b ">
+      <div className="flex-box align-center text-center h-10 items-end rounded-md bg-green-700 p-2 md:h-20 border-2 border-yellow-800">
+        <text className="font-semibold text-xl md:text-3xl text-slate-700">Western Pennsylvania Golf Course Ratings</text>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Featured Course</strong>
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Course Name Here as a link
             </a>
-            , brought to you by Vercel.
           </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span>
-          </Link>
+          
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
